f979182232367b54f0556c00c9e9f4e4f6297731
Jakub Beisert <Beisert97@gmail.com>
2022-11-10 18:40:46 +0100

    Update README.md

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import  DataRequired, EqualTo, ValidationError
 
 
 class RegistrationForm(FlaskForm):
-    login = StringField("Enter your login")
-    password = StringField("Enter your password")
+    login = StringField("Enter your login", validators=[DataRequired(message='Login can not be empty')])
+    password = StringField("Enter your password", validators=[DataRequired(message='Password can not be empty'), EqualTo('confirm', message='Password must match')])
+    confirm = StringField("Confirm your password", validators=[DataRequired(message='Please confirm password')])
     submit = SubmitField("Register")
