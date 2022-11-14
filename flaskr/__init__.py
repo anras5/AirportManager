112158c0a28c8697bb4ee404a930f875a279ca3b
anras5 <anras1filip@gmail.com>
2022-11-14 17:42:37 +0100

    Add favicon

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,7 @@ from flask_bootstrap import Bootstrap
 
 bootstrap = Bootstrap()
 
+
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
