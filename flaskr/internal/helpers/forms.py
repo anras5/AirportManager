8f9fe2aca9f3115ffc2a292fe1a685499a6d659e
anras5 <anras1filip@gmail.com>
2023-01-23 14:40:13 +0100

    Update forms

--- flaskr/internal/helpers/forms.py
+++ flaskr/internal/helpers/forms.py
@@ -1,13 +1,15 @@
+from datetime import datetime
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField, SelectField, IntegerField, DecimalField
+from wtforms import StringField, SubmitField, DateField, SelectField, IntegerField, DecimalField, PasswordField
 from wtforms.validators import DataRequired, ValidationError, Length, NumberRange, InputRequired
-from flaskr.internal.helpers.constants import MAX_NUMBER_9, MAX_NUMBER_12_6, MAX_NUMBER_6
+from flaskr.internal.helpers.constants import MAX_NUMBER_9, MAX_NUMBER_6
 
 
 def pesel_incorrect(form, field):
     for c in field.data:
         if c not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-            raise ValidationError("Wrong pesel")
+            raise ValidationError("Niepoprawny numer PESEL")
 
 
 # ------------------------------------------------------------------------------------------------------------------- #
@@ -26,10 +28,10 @@ class AirportForm(FlaskForm):
     icaocode = StringField("Podaj kod ICAO",
                            validators=[DataRequired(),
                                        Length(min=4, max=4, message="Kody ICAO mają 4 cyfry")])
-    longitude = DecimalField("Podaj długość geograficzną",
-                             validators=[DataRequired(), NumberRange(min=-180, max=180)])
-    latitude = DecimalField("Podaj szerokość geograficzną",
-                            validators=[DataRequired(), NumberRange(min=-90, max=90)])
+    longitude = DecimalField("Podaj długość geograficzną", places=3,
+                             validators=[NumberRange(min=-180, max=180), InputRequired()])
+    latitude = DecimalField("Podaj szerokość geograficzną", places=3,
+                            validators=[NumberRange(min=-90, max=90), InputRequired()])
     submit = SubmitField("Dodaj lotnisko")
 
 
@@ -44,8 +46,9 @@ class AirlinesForm(FlaskForm):
     nazwa = StringField("Podaj nazwę linii lotniczych",
                         validators=[DataRequired(),
                                     Length(min=1, max=100, message="Podaj od 1 do 100 znaków")])
-    kraj = StringField("Podaj kraj lotniska", validators=[DataRequired(),
-                                                          Length(min=1, max=25, message="Podaj od 1 do 25 znaków")])
+    kraj = StringField("Podaj kraj linii lotniczej", validators=[DataRequired(),
+                                                                 Length(min=1, max=25,
+                                                                        message="Podaj od 1 do 25 znaków")])
     submit = SubmitField("Dodaj linię lotniczą")
 
 
@@ -59,7 +62,7 @@ class AirlinesFormUpdate(AirlinesForm):
 class ManufacturersForm(FlaskForm):
     nazwa = StringField("Podaj nazwę producenta",
                         validators=[DataRequired(), Length(min=1, max=100, message="Podaj od 1 do 100 znaków")])
-    kraj = StringField("Podaj kraj lotniska",
+    kraj = StringField("Podaj kraj producenta",
                        validators=[DataRequired(), Length(min=1, max=25, message="Podaj od 1 do 25 znaków")])
     submit = SubmitField("Dodaj producenta")
 
@@ -75,11 +78,9 @@ class ModelsForm(FlaskForm):
     nazwa = StringField("Podaj nazwę modelu",
                         validators=[DataRequired(), Length(min=1, max=25, message="Podaj od 1 do 25 znaków")])
     liczba_miejsc = IntegerField("Podaj liczbę miejsc w samolocie",
-                                 validators=[DataRequired(),
-                                             NumberRange(min=0, max=MAX_NUMBER_9)])
+                                 validators=[NumberRange(min=0, max=MAX_NUMBER_9), InputRequired()])
     predkosc = IntegerField("Podaj maksymalną prędkość modelu",
-                            validators=[DataRequired(),
-                                        NumberRange(min=100, max=MAX_NUMBER_6)])
+                            validators=[NumberRange(min=100, max=MAX_NUMBER_6), InputRequired()])
     producent = SelectField("Wybierz producenta samolotu", validators=[DataRequired()])
     submit = SubmitField("Dodaj model")
 
@@ -95,9 +96,8 @@ class RunwaysForm(FlaskForm):
     nazwa = StringField("Podaj nazwę pasa startowego",
                         validators=[DataRequired(),
                                     Length(min=1, max=25, message="Podaj od 1 do 25 znaków")])
-    dlugosc = DecimalField("Podaj długość pasa w metrach",
-                           validators=[DataRequired(),
-                                       NumberRange(min=1, max=MAX_NUMBER_6)])
+    dlugosc = DecimalField("Podaj długość pasa w metrach", places=1,
+                           validators=[NumberRange(min=1, max=MAX_NUMBER_6), InputRequired()])
     opis = StringField("Podaj dodatkowy opis pasa startowego", validators=[Length(max=100)])
     submit = SubmitField("Dodaj pas")
 
@@ -115,8 +115,7 @@ class ArrivalForm(FlaskForm):
     model = SelectField("Wybierz model samolotu, który obsługuje połączenie", validators=[DataRequired()])
     linia_lotnicza = SelectField("Wybierz linię lotniczą, która obsługuje połączenie", validators=[DataRequired()])
     liczba_pasazerow = IntegerField("Podaj liczbę pasażerów, którzy przylecą tym lotem",
-                                    validators=[DataRequired(),
-                                                NumberRange(min=0, max=MAX_NUMBER_9)])
+                                    validators=[NumberRange(min=0, max=MAX_NUMBER_9), InputRequired()])
     submit = SubmitField("Dodaj przylot")
 
 
@@ -129,8 +128,7 @@ class ArrivalFormUpdateWithoutRunway(FlaskForm):
     model = SelectField("Wybierz model samolotu, który obsługuje połączenie", validators=[DataRequired()])
     linia_lotnicza = SelectField("Wybierz linię lotniczą, która obsługuje połączenie", validators=[DataRequired()])
     liczba_pasazerow = IntegerField("Podaj liczbę pasażerów, którzy przylecą tym lotem",
-                                    validators=[DataRequired(),
-                                                NumberRange(min=0, max=MAX_NUMBER_9)])
+                                    validators=[NumberRange(min=0, max=MAX_NUMBER_9), InputRequired()])
     submit = SubmitField("Edytuj przylot")
 
 
@@ -180,8 +178,8 @@ class ClassForm(FlaskForm):
                           validators=[DataRequired(), Length(min=1, max=25, message="Podaj od 1 do 25 znaków")])
     komfort = StringField("Podaj jaki komfort oferuje klasa",
                           validators=[DataRequired(), Length(min=1, max=25, message="Podaj od 1 do 25 znaków")])
-    cena = DecimalField("Podaj cenę biletu w tej klasie",
-                        validators=[DataRequired(), NumberRange(min=0, max=MAX_NUMBER_6)])
+    cena = DecimalField("Podaj cenę biletu w tej klasie", places=2,
+                        validators=[NumberRange(min=0, max=MAX_NUMBER_6), InputRequired()])
     submit = SubmitField("Dodaj klasę")
 
 
@@ -195,15 +193,19 @@ class ClassFormUpdate(ClassForm):
 class PassengerForm(FlaskForm):
     login = StringField("Podaj login",
                         validators=[DataRequired(), Length(min=1, max=25, message="Podaj od 1 do 25 znaków")])
-    haslo = StringField("Podaj hasło",
-                        validators=[DataRequired(), Length(min=1, max=25, message="Podaj od 1 do 25 znaków")])
+    haslo = PasswordField("Podaj hasło",
+                          validators=[DataRequired(), Length(min=1, max=25, message="Podaj od 1 do 25 znaków")])
     imie = StringField("Podaj imię",
                        validators=[DataRequired(), Length(min=1, max=25, message="Podaj od 1 do 25 znaków")])
     nazwisko = StringField("Podaj nazwisko",
                            validators=[DataRequired(), Length(min=1, max=25, message="Podaj od 1 do 25 znaków")])
-    pesel = StringField("Podaj pesel", validators=[DataRequired(), Length(min=11, max=11)])
+    pesel = StringField("Podaj pesel", validators=[DataRequired(), Length(min=11, max=11), pesel_incorrect])
     data_urodzenia = DateField("Podaj datę urodzin", format="%Y-%m-%d", validators=[DataRequired()])
     submit = SubmitField("Dodaj pasażera")
+
+    def validate_data_urodzenia(form, field):
+        if field.data < datetime(year=1900, month=1, day=1).date():
+            raise ValidationError("Data urodzenia musi być większa niż 01-01-1900")
 
 
 class PassengerFormUpdate(PassengerForm):
